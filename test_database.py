22788a198b17738cdd903d6c4322499bd145f728
Noah Knox <leonidas098@yahoo.com>
2019-11-23 18:53:21 +0000

    changed function name

--- test_database.py
+++ test_database.py
@@ -9,7 +9,7 @@ def create_tables():
     FirstName VARCHAR(20) NOT NULL,
     LastName VARCHAR(20) NOT NULL)""")
 
-def check_if_username_exists(username):
+def check_username(username):
     database = sqlite3.connect("database.db")
     c = database.cursor()
     while True:
