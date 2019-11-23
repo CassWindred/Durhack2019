054c38ff86d45d53711c691d4e9d7ad1ecf8f523
Cass Windred <qwerto227@gmail.com>
2019-11-23 21:18:06 +0000

    Added Places API to Javascript and removed SQLite from requirements

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from app import app
-
 from flask import render_template
 
+import database_manager
 
 @app.route('/example')
 def example():
@@ -13,6 +13,7 @@ def hello_world():
     user = {'username': 'Miguel'}
     return render_template('mainpage.html', title='Home', user=user)
 
+
 @app.route('/getPlaceInfo/<placeId>')
 def getPlaceInfo(placeId):
-    pass
+    database_manager.place_access(placeId)
