ffc9ed6b33aafeb857475a9479e3b29f072c0c05
Katie <katie.midgley@ntlworld.com>
2019-11-23 20:01:44 +0000

    Login page form and base template update

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,15 @@
 from app import app
+from flask import render_template, flash, redirect, url_for
+from app.forms import LoginForm
 
-from flask import render_template
-
-
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}'.format(
+            form.username.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/index')
 def index():
