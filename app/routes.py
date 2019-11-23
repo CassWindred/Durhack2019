2d42ec630630781cce446eca7580ca711e48b4d6
Cass Windred <qwerto227@gmail.com>
2019-11-23 14:27:35 +0000

    Rearranged and added Mainpage.html

---
+++ app/routes.py
@@ -0,0 +1,14 @@
+from app import app
+
+from flask import render_template
+
+
+@app.route('/example')
+def example():
+    return "This is an example page!"
+
+
+@app.route('/')
+def hello_world():
+    user = {'username': 'Miguel'}
+    return render_template('mainpage.html', title='Home', user=user)
