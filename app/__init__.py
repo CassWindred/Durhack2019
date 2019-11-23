f13bf16ce49224b641fa7b5d0ae219ebd35a1878
Noah Knox <leonidas098@yahoo.com>
2019-11-23 15:35:02 +0000

    Auto stash before merge of "master" and "origin/master"

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
-
+from flask.ext.googlemaps import GoogleMaps as gmaps
 
 app = Flask(__name__)
 
 bootstrap = Bootstrap(app)
+gmaps(app)
 from app import routes
